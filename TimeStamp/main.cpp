9e1d33d7a00f85af121e25078f2b62c4bc9fa9d3
Tabassum Bari <tabassum.bari@northsouth.edu>
2023-04-18 21:19:13 +0600

    Main File
    Contains: Inserting 5 data , deleting one and printing all of them

--- TimeStamp/main.cpp
+++ TimeStamp/main.cpp
@@ -1,9 +1,59 @@
 #include <iostream>
-
+#include"TimeStamp.h"
+#include"TimeStamp.cpp"
 using namespace std;
+
+class TimeStamp{
+private:
+    int seconds,minutes,hours;
+public:
+    TimeStamp(int s=0,int m=0,int h=0)
+    {
+        seconds=s;
+        minutes=m;
+        hours=h;
+
+
+
+    }
+    void print(){
+    cout<<hours<<":"<<minutes<<":"<<seconds<<endl;
+    }
+bool operator==(const TimeStamp& time2)
+{
+    return (seconds==time2.seconds&& minutes==time2.minutes&& hours==time2.hours);
+
+}
+};
+
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    TimeStamp time[5]={ TimeStamp(23,34,23),TimeStamp(02,13,13),TimeStamp(12,45,43),TimeStamp(17,36,26),TimeStamp(20,02,52)};
+
+    //delete 25 36 17
+    for(int i=0;i<5;i++)
+    {
+        if(time[i]==TimeStamp(17,36,25))
+        {
+            time[i]=TimeStamp();
+            break;
+        }
+
+    }
+
+    //print the list
+    for(int i=0;i<5;i++)
+    {
+        if(time[i].print)
+    {
+       time[i].print();
+    }
+
+    }
+
+
+return 0;
+
 }
