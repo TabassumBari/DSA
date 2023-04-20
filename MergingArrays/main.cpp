177a657ba062c076b9ae91129b20e53e86cffc9d
Tabassum Bari <tabassum.bari@northsouth.edu>
2023-04-20 10:50:14 +0600

    Merging Two Arrays In Ascending Order
    It has a function to merge two arrays it works in binary search method to organize the values

--- MergingArrays/main.cpp
+++ MergingArrays/main.cpp
@@ -1,9 +1,62 @@
 #include <iostream>
-
+#include"Merged.h"
+#include"Merged.cpp"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  void mergeArray(int ar1[],int ar2[],int m,int n,int merged[])
+{
+
+    int i=0;
+    int j=0;
+    int k=0;
+    while(i<m&&j<n)
+    {
+        if(ar1[i]<ar2[j])
+
+        {
+        merged[k]=ar1[i];
+        i++;
+        }
+
+       else
+       {
+        merged[k]=ar2[j];
+        j++;
+       }
+       k++;
+
+    }
+    while(i<m)
+{
+    merged[k]=ar1[i];
+    i++;
+    k++;
+
+}
+while(j<n)
+{
+    merged[k]=ar2[j];
+    j++;
+    k++;
+}
+}
+
+
+
+int main()
+{
+  int ar1[]={1,5,9,10,11,20};
+  int ar2[]={4,6,7,13,22,30,40};
+  int m=sizeof(ar1)/sizeof(ar1[0]);
+  int n=sizeof(ar2)/sizeof(ar2[0]);
+  int merged[m+n];
+  mergeArray(ar1,ar2,m,n,merged);
+  cout<<"The two arrays merged are:"<<endl;
+  for(int i=0;i<m+n;i++)
+  {
+      cout<<merged[i]<<" ";
+  }
+}
 }
