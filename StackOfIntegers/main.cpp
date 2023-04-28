91941fd50803123bf63caf17c6b07076cf9f2090
Tabassum Bari <tabassum.bari@northsouth.edu>
2023-04-28 22:26:10 +0600

    A stack of integers
    Creating a simple stack of integers and executing various functions in it.

---
+++ StackOfIntegers/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include"IntegerStack.h"
+using namespace std;
+
+
+int main()
+{
+ Stack stack1;
+
+ if(stack1.IsEmpty())
+ {
+     cout<<"Stack is empty"<<endl;
+ }
+
+ stack1.Push(5);
+ stack1.Push(7);
+ stack1.Push(4);
+ stack1.Push(2);
+ if(!stack1.IsEmpty())
+ {
+     cout<<"Stack is not empty"<<endl;
+ }
+ else if(!stack1.IsFull())
+ {
+     cout<<"Stack is not full"<<endl;
+}
+cout<<"Stack values"<<endl;
+while(!stack1.IsEmpty())
+      {
+            cout<<stack1.Top()<<"";
+            stack1.Pop();
+
+      }
+if(stack1.IsFull())
+{
+    cout<<"Stack is full"<<endl;
+
+}
+stack1.Pop();
+stack1.Pop();
+cout<<"Top item of the stack"<<stack1.Top()<<endl;
+return 0;
+}
