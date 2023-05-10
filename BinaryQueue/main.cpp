5ab969a405a75ceedbd3cf23aec664888cf81619
Tabassum Bari <tabassum.bari@northsouth.edu>
2023-05-10 19:10:04 +0600

    Binary Queue
    A C++ code that turns a decimal number from input to a binary number by using queue data structure

--- BinaryQueue/main.cpp
+++ BinaryQueue/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-
+#include <queue>
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+void printBinaryNum(int n) {
+    queue<string> Q;
+    Q.push("1");
+
+    for (int i = 0; i < n; i++) {
+        string binaryNumber = Q.front();
+        Q.pop();
+        cout << binaryNumber << endl;
+
+        Q.push(binaryNumber + "0");
+        Q.push(binaryNumber + "1");
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    printBinaryNum(n);
+
     return 0;
 }
