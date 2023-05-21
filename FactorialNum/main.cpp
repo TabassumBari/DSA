15e05085fa202ea2b3c2b7fa195d056db62270ab
Tabassum Bari <tabassum.bari@northsouth.edu>
2023-05-21 21:51:41 +0600

    Factorial of a Number
    A C++ code that finds out factorial of a number using recursion.

--- FactorialNum/main.cpp
+++ FactorialNum/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+
+    int result = factorial(num);
+    std::cout << "Factorial of " << num << " is: " << result << std::endl;
+
     return 0;
 }
+
